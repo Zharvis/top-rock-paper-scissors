75287fdbe2480244f042d0a48afc26ec30757879
fmrc <fatih.mehmed.cin@gmail.com>
2022-07-06 16:10:43 +0200

    feat: remove old console implementation

--- app/script.js
+++ app/script.js
@@ -8,15 +8,6 @@ function computerPlay() {
   return Math.floor(Math.random() * 3);
 }
 
-function playerPlay() {
-  let play = 10;
-  while (play < 0 || play > 2) {
-    play = prompt("Choose your next play. (1:rock, 2:paper, 3:scissors)");
-    play--;
-  }
-  return play;
-}
-
 function playRound(playerSelection, computerSelection) {
   let result = "gg";
   if (playerSelection === computerSelection) {
@@ -49,46 +40,6 @@ function playRound(playerSelection, computerSelection) {
     }
   }
   return result;
-}
-
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  while (playerScore < 3 && computerScore < 3) {
-    let playerSelection = playerPlay();
-    let computerSelection = computerPlay();
-    let result = playRound(playerSelection, computerSelection);
-
-    switch (result) {
-      case "tie":
-        console.log(`GG, you both had ${plays[playerSelection]}. It's a tie.`);
-        break;
-
-      case "win":
-        console.log(
-          `You win! ${plays[playerSelection]} beats ${plays[computerSelection]}`
-        );
-        playerScore++;
-        break;
-
-      case "loss":
-        console.log(
-          `You lose... ${plays[playerSelection]} loses to ${plays[computerSelection]}`
-        );
-        computerScore++;
-        break;
-    }
-  }
-  if (playerScore === 3) {
-    console.log(
-      `You beat the computer! Congratulations. (${playerScore}:${computerScore})`
-    );
-  } else {
-    console.log(
-      `You lost to the almighty Computer Baba...(${playerScore}:${computerScore})`
-    );
-  }
 }
 
 let playerScore = 0;
